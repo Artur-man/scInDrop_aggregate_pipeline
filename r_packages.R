bc867b4c8475009aed4736894f9f04a679be0f5f
Artur Manukyan <artur-man@hotmail.com>
2022-06-26 16:45:26 -0400

    Create r_packages.R

---
+++ r_packages.R
@@ -0,0 +1,5 @@
+# Bioconductor
+install.packages("BiocManager", repos='http://cran.us.r-project.org')
+library(BiocManager)
+install.packages("ggplot2", repos='http://cran.us.r-project.org')
+install.packages("dplyr", repos='http://cran.us.r-project.org')
